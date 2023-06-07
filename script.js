0b1ba7ca78c5a4e8208c00996cc5e5896005b453
DIEGO DE ALMEIDA RAMOS <diegoalmeidapinduca@gmail.com>
2023-06-06 23:00:36 -0300

    projeto atualizado

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function alterarBotao(){
     const html = document.documentElement
+    html.classList.toggle("light")
 
     /*if(body.classList.contains('light')){
         html.classList.remove('light')
@@ -7,5 +8,25 @@ function alterarBotao(){
     else{
         html.classList.add('light')
     }*/
-    html.classList.toggle('light')
+
+
+    // pegar a tag imagem
+
+    const img = document.querySelector("#profile img")
+
+     //substituir a imagem
+    //se tiver light mode , adcionar a imagem light
+    if(html.classList.contains("light")){
+        img.setAttribute("src", "./images/avatar-light-mode.png")
+
+    }
+    //se nao, manter a imagem normal
+    else{
+        img.setAttribute("src","./images/Avatar.png")
+
+    }
+   
+       
+
+    
 }
